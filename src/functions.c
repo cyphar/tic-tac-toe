f4226ae7abf56cde193866d656dee4465a671d10
cyphar <cyphar@cyphar.com>
2013-01-24 20:13:53 +1100

    relicensed under MIT (because it is more permissive)

--- src/functions.c
+++ src/functions.c
@@ -1,20 +1,24 @@
 /* 
  * Tic-Tac-Toe: A heuristic implementation of the classic game.
- * Copyright (C) 2013 Cyphar <public@cyphar.com>
+ * Copyright (c) 2012, 2013 Cyphar
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 1. The above copyright notice and this permission notice shall be included in 
+ *    all copies or substantial portions of the Software.
  *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 #include <stdio.h>	/* fprintf(), printf() */
@@ -42,23 +46,28 @@
 		"  -v, --version         Show version information\n" \
 		"  -l, --license         Show license information\n" \
 		"\n" \
-		"Report bugs to <public@cyphar.com>.\n", __progname
+		"Report bugs to <bugs@cyphar.com>\n", __progname
 
 #define LICENSE	"Tic-Tac-Toe: A heuristic implementation of the classic game.\n" \
-		"Copyright (C) 2013 Cyphar <public@cyphar.com>\n" \
+		"Copyright (c) 2012, 2013 Cyphar\n" \
 		"\n" \
-		"This program is free software: you can redistribute it and/or modify\n" \
-		"it under the terms of the GNU General Public License as published by\n" \
-		"the Free Software Foundation, either version 3 of the License, or\n" \
-		"(at your option) any later version.\n" \
+		"Permission is hereby granted, free of charge, to any person obtaining a copy of\n" \
+		"this software and associated documentation files (the \"Software\"), to deal in\n" \
+		"the Software without restriction, including without limitation the rights to\n" \
+		"use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of\n" \
+		"the Software, and to permit persons to whom the Software is furnished to do so,\n" \
+		"subject to the following conditions:\n" \
 		"\n" \
-		"This program is distributed in the hope that it will be useful,\n" \
-		"but WITHOUT ANY WARRANTY; without even the implied warranty of\n" \
-		"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n" \
-		"GNU General Public License for more details.\n" \
+		"1. The above copyright notice and this permission notice shall be included in\n" \
+		"   all copies or substantial portions of the Software.\n" \
 		"\n" \
-		"You should have received a copy of the GNU General Public License\n" \
-		"along with this program.  If not, see <http://www.gnu.org/licenses/>.\n"
+		"THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n" \
+		"IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n" \
+		"FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n" \
+		"AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n" \
+		"LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n" \
+		"OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n" \
+		"SOFTWARE.\n"
 
 #define ANSI_COLOUR_GREEN	"\e[1;32m"
 #define WARGAMES		"\e[1;96m"
